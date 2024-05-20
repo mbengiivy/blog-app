520aafb29f3c672a9ba048fa1be6bef5c8a66ea7
mbengi <mbengiivy@gmail.com>
2024-05-20 18:06:20 +0000

    Describe the changes made to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,6 @@ STATIC_ROOT = BASE_DIR/ 'STATIC'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR / 'media')
