a0f614b73fd96dcffdac609f7a29efdcad2ff103
Mbengi <mbengiivy@gmail.com>
2024-05-22 15:42:39 +0300

    Added a debugging line to ensure blogs exist

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print(posts)  # Debugging line
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
